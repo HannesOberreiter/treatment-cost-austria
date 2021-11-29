e0fb638ee2d46c4adbba9d00e4acf0162759e4c8
HannesOberreiter <hoberreiter@gmail.com>
2021-11-29 13:16:14 +0100

    push

--- src/r-model.R
+++ src/r-model.R
@@ -1,5 +1,5 @@
 # Description -------------------------------------------------------------
-# Simple multiple regression linear model for evulation independence of our factors
+# Simple multiple regression linear model for evaluate influence and independence of our predictors
 # For random forest and other stuff look at the playground folder, model.Rmd
 library(tidymodels)
 library(vip)
@@ -11,13 +11,13 @@ r_model <- list()
 r_model$zero_expenses <- dfClean %>%
     filter(costs == 0) %>%
     nrow()
+
 r_model$data <- dfClean %>%
     filter(costs != 0) %>%
+    # because we log transform
     # filter(costs < 100) %>%
     filter(op_cert_org_beek %in% c("Ja", "Nein") & op_migratory_beekeeper %in% c("Ja", "Nein")) %>%
     mutate(
-        # log for somewhat normal distribution
-        costs = log(costs),
         op_cert_org_beek = forcats::as_factor(op_cert_org_beek),
         op_migratory_beekeeper = forcats::as_factor(op_migratory_beekeeper),
     ) %>%
@@ -31,20 +31,51 @@ r_model$data_split <- rsample::initial_split(r_model$data, prop = 9 / 10, strata
 r_model$train_data <- rsample::training(r_model$data_split)
 r_model$test_data <- rsample::testing(r_model$data_split)
 
+# QQ Plots ------------------------------
+# for M & M section to show how good the transformation is
+p1 <- r_model$data %>%
+    ggplot(aes(sample = log(costs))) +
+    geom_qq() +
+    geom_qq_line() +
+    ylab(TeX("Expenses/Colony \\[$\\log_{10}$ Euro\\]"))
+
+p2 <- r_model$data %>%
+    ggplot(aes(sample = log(hives_winter))) +
+    geom_qq() +
+    geom_qq_line() +
+    ylim(-2, 6.5) +
+    ylab(TeX("Operation Size / Number of Colonies \\[$\\log_{10}$ #\\]"))
+
+p <- (p1 | p2) +
+    patchwork::plot_annotation(tag_levels = "A") &
+    xlab("Theoretical Normal Distribution Quantiles") &
+    scale_y_continuous(
+        breaks = c(-2, 0, 2, 4, 6),
+        limits = c(-2, 6)
+    ) &
+    scale_x_continuous(
+        breaks = c(-4, -2, 0, 2, 4),
+        limits = c(-4, 4)
+    )
+
+fSaveImages(p, "model-qq", w = 8, h = 5)
+
 # Recipes ------------------------------
 r_model$rec_intercept_only <-
     recipe(costs ~ 1, data = r_model$train_data) %>%
-    # step_log(hives_winter, base = 10) %>%
+    step_log(costs, base = 10, skip = TRUE) %>%
     prep(training = r_model$train_data)
 
 r_model$rec_base <-
     recipe(costs ~ hives_winter, data = r_model$train_data) %>%
     step_log(hives_winter, base = 10) %>%
+    step_log(costs, base = 10, skip = TRUE) %>%
     prep(training = r_model$train_data)
 
 r_model$rec_operational <-
     recipe(costs ~ hives_winter + op_cert_org_beek + op_migratory_beekeeper, data = r_model$train_data) %>%
     step_log(hives_winter, base = 10) %>%
+    step_log(costs, base = 10, skip = TRUE) %>%
     prep(training = r_model$train_data)
 
 r_model$rec_treatment <-
@@ -52,6 +83,7 @@ r_model$rec_treatment <-
         T_lactic_total_yn + T_oxalic_trickle_pure_total_yn + T_oxalic_vapo_total_yn + T_oxalic_trickle_mix_total_yn +
         T_thymol_total_yn + T_synthetic_total_yn + T_other_total_yn + hives_winter + op_cert_org_beek + op_migratory_beekeeper, data = r_model$train_data) %>%
     step_log(hives_winter, base = 10) %>%
+    step_log(costs, base = 10, skip = TRUE) %>%
     prep(training = r_model$train_data)
 
 r_model$rec_state <-
@@ -59,6 +91,7 @@ r_model$rec_state <-
         T_lactic_total_yn + T_oxalic_trickle_pure_total_yn + T_oxalic_vapo_total_yn + T_oxalic_trickle_mix_total_yn +
         T_thymol_total_yn + T_synthetic_total_yn + T_other_total_yn + hives_winter + op_cert_org_beek + op_migratory_beekeeper + state, data = r_model$train_data) %>%
     step_log(hives_winter, base = 10) %>%
+    step_log(costs, base = 10, skip = TRUE) %>%
     prep(training = r_model$train_data)
 
 # Models -----------------------------
@@ -87,9 +120,7 @@ r_model$fitted <- r_model$data_wflows %>%
     unnest() %>%
     mutate(
         fitted = map(workflow, fit, data = r_model$train_data)
-    )
-
-r_model$fitted <- r_model$fitted %>%
+    ) %>%
     mutate(
         stat_parsnip = map(fitted, extract_fit_parsnip),
         stat_tidy = map(stat_parsnip, tidy),
@@ -97,7 +128,40 @@ r_model$fitted <- r_model$fitted %>%
     ) %>%
     unnest(stat_values)
 
+# Manually selected best model based on AIC and BIC
+# filter(AIC == min(AIC) & BIC == min(BIC))
 (r_model$best_model <- r_model$fitted$fitted[[4]])
+
+# Model Residuals Distribution ------------------------
+p <- pull_workflow_fit(r_model$best_model)$fit$residuals %>%
+    as_tibble() %>%
+    ggplot(aes(sample = value)) +
+    geom_qq() +
+    geom_qq_line() +
+    labs(
+        x = "Theoretical Quantiles (Normal Distribution)",
+        y = "Training Dataset - Model Residuals Quantiles"
+    )
+
+fSaveImages(p, "model-qq-residuals", w = 8, h = 5)
+
+# Performance Tests ------------------------
+# library(performance)
+# m <- extract_model(r_model$best_model)
+## m1 <- extract_model(r_model$fitted$fitted[[1]])
+## m2 <- extract_model(r_model$fitted$fitted[[2]])
+## m3 <- extract_model(r_model$best_model)
+# performance::check_collinearity(m) # looks good
+# performance::check_autocorrelation(m) # looks good
+# performance::check_heteroscedasticity(m) # http://www.statsmakemecry.com/smmctheblog/confusing-stats-terms-explained-heteroscedasticity-heteroske.html
+# performance::check_homogeneity(m, method = "auto") # looks good
+# performance::check_normality(m) # fails but test will most often result in non-normality
+# performance::check_outliers(m) # ok
+# performance::performance_accuracy(m) # we could report this
+# performance::test_performance(m2, m3, denominator = m1)
+# bayestestR::bayesfactor_models(m2, m3, denominator = m1)
+
+# Vi Scores ------------------------
 
 # Remove intercept only model otherwise we get an error
 r_model$vi_scores <- r_model$fitted[2:5, ] %>%
@@ -109,9 +173,62 @@ r_model$vi_scores <- r_model$fitted[2:5, ] %>%
         )
     )
 
-pull_workflow_fit(r_model$best_model) %>%
-    vi()
+# pull_workflow_fit(r_model$best_model) %>%
+#    vi()
 
+# Coefficients ------------------------
+r_model$coeff <- extract_fit_parsnip(r_model$best_model) %>%
+    tidy() %>%
+    mutate(
+        name = str_remove_all(term, "_yn1"),
+        estimate_conv = 10^abs(estimate) * ifelse(estimate < 0, -1, 1),
+        error_conv = 10^std.error,
+        direction = ifelse(estimate < 0, "Negative", "Positive"),
+        direction = ifelse(name == "(Intercept)", "Neutral", direction)
+    ) %>%
+    left_join(treatmentList[2:3], by = c("name" = "ttotal")) %>%
+    mutate(
+        tname = case_when(
+            name == "(Intercept)" ~ "(Intercept)",
+            name == "hives_winter" ~ "Number of colonies",
+            name == "op_cert_org_beekJa" ~ "Certified Organic",
+            name == "op_migratory_beekeeperJa" ~ "Migratory Beekeeper",
+            TRUE ~ paste0("(T) ", tname)
+        ),
+        tname = forcats::fct_reorder(tname, estimate)
+    )
+
+r_model$coeff_intercept <- r_model$coeff %>%
+    filter(name == "(Intercept)") %>%
+    pull(estimate_conv, error_conv)
+
+p <- r_model$coeff %>%
+    filter(name != "(Intercept)") %>%
+    ggplot(aes(x = estimate_conv, y = tname, color = direction)) +
+    geom_vline(xintercept = 0, linetype = "dashed") +
+    geom_pointrange(
+        aes(
+            xmin = estimate_conv - error_conv,
+            xmax = estimate_conv + error_conv
+        ),
+        show.legend = FALSE
+    ) +
+    scale_x_continuous(
+        breaks = scales::pretty_breaks()
+    ) +
+    scale_colour_manual(
+        values = c("#D55E00", "#009E73")
+    ) +
+    xlab("Estimate + Standard Error [Euro]") +
+    ylab("Coefficients") +
+    theme(
+        panel.grid.major.y = element_line(),
+        panel.grid.major.x = element_line()
+    )
+
+fSaveImages(p, "model-whisker", w = 8, h = 5)
+
+# Other Model Params Extractions ------------------------
 r_model$pvalues <- r_model$fitted %>%
     select(wflow_id, fitted) %>%
     mutate(
@@ -128,12 +245,17 @@ r_model$prediction <-
     r_model$fitted %>%
     select(wflow_id, fitted) %>%
     mutate(
-        prediction_test = map(fitted, ~ augment(.x, r_model$test_data) %>%
-            select(costs, .pred) %>%
-            mutate(type = "Testing")),
-        prediction_train = map(fitted, ~ augment(.x, r_model$train_data) %>%
-            select(costs, .pred) %>%
-            mutate(type = "Training"))
+        prediction_test = map(fitted, ~ tibble(.fitted = predict(.x, r_model$test_data, type = "raw"), costs = r_model$test_data$costs) %>%
+            mutate(
+                type = "Testing",
+                costs = log(costs)
+            )),
+        prediction_train = map(fitted, ~ augment(extract_model(.x), r_model$train_data) %>%
+            select(costs, .fitted) %>%
+            mutate(
+                type = "Training",
+                costs = log(costs)
+            ))
     )
 
 r_model$performance_accuracy <- r_model$fitted[2:5, ] %>%
@@ -145,50 +267,38 @@ r_model$performance_accuracy <- r_model$fitted[2:5, ] %>%
         )
     )
 
-r_model$performance_accuracy$accuracy[[3]]["Accuracy"]
+r_model$performance_accuracy_best <- r_model$performance_accuracy %>%
+    filter(wflow_id == "Treatment_lm") %>%
+    pull(accuracy) %>%
+    pluck(1)
 
 r_model$prediction_stats <- bind_rows(
     r_model$prediction %>% filter(wflow_id == "Treatment_lm") %>% unnest(prediction_test) %>%
-        yardstick::rmse(., costs, .pred) %>% mutate(type = "Training"),
+        yardstick::rmse(., costs, .fitted) %>% mutate(type = "Training"),
     r_model$prediction %>% filter(wflow_id == "Treatment_lm") %>% unnest(prediction_train) %>%
-        yardstick::rmse(., costs, .pred) %>% mutate(type = "Test"),
+        yardstick::rmse(., costs, .fitted) %>% mutate(type = "Testing"),
 )
 
 p <- bind_rows(r_model$prediction %>% unnest(prediction_test), r_model$prediction %>% unnest(prediction_train)) %>%
     filter(wflow_id %in% c("Intercept_lm", "Treatment_lm")) %>%
     mutate(wflow_id = ifelse(wflow_id == "Intercept_lm", "Intercept Only", "Best Model")) %>%
-    ggplot(aes(costs, .pred, color = wflow_id, group = wflow_id)) +
+    left_join(r_model$prediction_stats) %>%
+    mutate(label = glue("{type} (RMSE: {round(.estimate,2)})")) %>%
+    ggplot(aes(costs, .fitted, color = wflow_id, group = wflow_id)) +
     ylab("Prediction [log(Euro)]") +
     xlab("Survey [log(Euro)]") +
     geom_point(alpha = .15, show.legend = FALSE) +
     geom_smooth(method = "lm") +
     geom_abline(color = colorBlindBlack8[8]) +
-    labs(color = "Model") +
+    labs(color = "") +
     ggplot2::scale_color_manual(values = colorBlindBlack8[1:2]) +
     coord_obs_pred() +
-    facet_wrap(~type) +
+    facet_wrap(~label) +
+    # https://stackoverflow.com/questions/21066077/remove-fill-around-legend-key-in-ggplot
+    guides(color = guide_legend(override.aes = list(fill = NA))) +
     ggplot2::theme(
         legend.position = "top"
     )
-
 fSaveImages(p, "model-pred", w = 8, h = 5)
 
-
 saveRDS(r_model, "output/r-model.rds")
-
-# Should report some perfomrance tests?
-library(performance)
-m <- extract_model(r_model$best_model)
-
-m1 <- extract_model(r_model$fitted$fitted[[1]])
-m2 <- extract_model(r_model$fitted$fitted[[2]])
-m3 <- extract_model(r_model$best_model)
-performance::check_collinearity(m) # looks good
-performance::check_autocorrelation(m) # looks good
-performance::check_heteroscedasticity(m) # http://www.statsmakemecry.com/smmctheblog/confusing-stats-terms-explained-heteroscedasticity-heteroske.html
-performance::check_homogeneity(m, method = "auto") # looks good
-performance::check_normality(m) # fails but test will most often result in non-normality
-performance::check_outliers(m) # ok
-performance::performance_accuracy(m) # we could report this
-performance::test_performance(m2, m3, denominator = m1)
-bayestestR::bayesfactor_models(m2, m3, denominator = m1)
